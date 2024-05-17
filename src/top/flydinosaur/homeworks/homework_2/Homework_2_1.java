df3b5cf2313a185bc972d0bc29a75bd9eb2a7fc1
FlyDinosaur <3457552420@qq.com>
2024-05-17 11:26:15 +0800

    upadate

---
+++ src/top/flydinosaur/homeworks/homework_2/Homework_2_1.java
@@ -0,0 +1,19 @@
+package top.flydinosaur.homeworks.homework_2;
+
+//从键盘输入5个整数，求和并输出结果。提示：使用循环读取5个int类型的数据。
+
+import java.util.Scanner;
+
+public class Homework_2_1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            sum += scanner.nextInt();
+        }
+        System.out.println("5个数的和是：" + sum);
+    }
+
+}
+
